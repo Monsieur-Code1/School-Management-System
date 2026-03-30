382cd86fa5b4ec24ed91f7bbf76a99f4e26fc6a8
sally <sallymohammed1521999@gmail.com>
2026-03-30 22:23:16 +0200

    feat: implement dynamic routing and update dashboard layout with active state

--- scripts/dashboard/script.js
+++ scripts/dashboard/script.js
@@ -1,22 +1,88 @@
-"use strict"
-const statsContainer = document.querySelector(".stats-container");
+const sidebarLink = document.querySelector(".sidebar-link");
 
-const statsData =[
-    { title: "Students", value: "100", colorClass: "students-cart", footerColor: "students-footer"},
-    { title: "Teachers", value: "50", colorClass: "teachers-cart", footerColor: "teachers-footer"},
-    { title: "Semester", value: "3", colorClass: "semester-cart", footerColor: "semester-footer"}
-]
+const sidebarItems = [
+  { path: "/", name: "Dashboard" },
+  { path: "/student", name: "Student" },
+  { path: "/staff", name: "Staff" },
+  { path: "/academics", name: "Academics" },
+  { path: "/performance", name: "Performance" },
+  { path: "/collectFees", name: "Collect Fees" },
+  { path: "/announcement", name: "Announcement" },
+  { path: "/setup", name: "Setup" },
+  { path: "/changePassword", name: "Change password" },
+];
 
-statsContainer.innerHTML= statsData.map((item) =>{
+sidebarLink.innerHTML = sidebarItems
+  .map((item) => {
     return `
-    <div class="${item.colorClass}">
-            <div class="stat-content">
-                <h3>${item.title}</h3>
-                <p class="stat-number">${item.value}</p>
-            </div>
-            <div class="stat-footer ${item.footerColor}">
-                <a href="#"> <img src="../../img/Group 7.svg" alt="icon"> </a>
-            </div>
-        </div>
-    `
-}).join("")
+    <li>
+        <a class="nav-link" href="${item.path}" onclick="route(event)">${item.name}</a>
+    </li>
+    `;
+  })
+  .join("");
+
+const router = {
+  "/": {
+    html: "/pages/dashboard/dashboard.html",
+    js: "/scripts/dashboard/dashboard.js",
+    css: "/style/dashboard/dashboard.css",
+  },
+  "/student": { html: "/pages/dashboard/student.html" },
+  "/staff": { html: "/pages/dashboard/staff.html" },
+  "/academics": { html: "/pages/dashboard/academics.html" },
+  "/performance": { html: "/pages/dashboard/performance.html" },
+  "/collectFees": { html: "/pages/dashboard/collectfees.html" },
+  "/announcement": { html: "/pages/dashboard/announcement.html" },
+  "/setup": { html: "/pages/dashboard/setup.html" },
+  "/changePassword": { html: "/pages/dashboard/changepassword.html" },
+};
+
+const route = (event) => {
+  event.preventDefault();
+  window.history.pushState({}, "", event.currentTarget.href);
+  handleLocation();
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  const route = router[path] || router["/"];
+  const pageTitle =
+    sidebarItems.find((item) => item.path === path)?.name || "Dashboard";
+  const displaySpan = document.getElementById("page-title-display");
+
+  if (displaySpan) {
+    displaySpan.innerText = pageTitle ? pageTitle : "Dashboard";
+  }
+
+  document.querySelectorAll(".nav-link").forEach((link) => {
+    link.classList.toggle("active", link.getAttribute("href") === path);
+  });
+
+  try {
+    let dynamicStyle = document.getElementById("dynamic-style");
+    const res = await fetch(route.html);
+    const html = await res.text();
+    document.querySelector(".main-pages").innerHTML = html;
+
+    if (route.js) {
+      const module = await import(`${route.js}?=${Date.now()}`);
+      if (module.init) module.init();
+    }
+    if (!dynamicStyle) {
+      dynamicStyle = document.createElement("link");
+      dynamicStyle.id = "dynamic-style";
+      dynamicStyle.rel = "stylesheet";
+      document.head.appendChild(dynamicStyle);
+    }
+
+    if (route.css) {
+      dynamicStyle.href = route.css;
+    }
+  } catch {
+    console.log("error");
+  }
+};
+
+window.onpopstate = handleLocation;
+handleLocation();
